b07793753e45f893de8afc10fd6c8ad142945ae2
AFSAL P U <afsalpu16@gmail.com>
2023-05-21 15:54:47 +0530

    create-campaign page design completed

---
+++ client/src/context/index.js
@@ -0,0 +1,9 @@
+import react, { useContext, createContext } from 'react'
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react'
+import { ethers } from 'ethers'
+
+const StateContext = createContext()
+
+export const StateContextProvider = ({ children }) => {
+    const { contract } = useContract('')
+}
