6d98212978cbff48e8f2d81c78a9414f3a10b6f5
AFSAL P U <afsalpu16@gmail.com>
2023-05-23 21:06:00 +0530

    Home and Profile page completed

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { ethers } from 'ethers'
+
+import { useStateContext } from '../context'
+import { CustomButton } from '../components'
+import { calculateBarPercentage, daysLeft } from '../utils'
+import { thirdweb } from '../assets'
 
 const CampaignDetails = () => {
   return (
